8fdeeba860e5681b1d8a70d48aecf6e34b1379b4
Patrick Moberg <patrickmoberg@gmail.com>
2019-01-07 23:06:29 -0500

    proof of concept

--- js/main.js
+++ js/main.js
@@ -1,15 +1,7 @@
 //create a synth and connect it to the master output (your speakers)
 var synth = new Tone.Synth().toMaster();
 
-$.getJSON( "/sample_data.json", function( data ) {
-
-	$.each( data.list, function( key, val ) {
-	    console.log(val.main.temp);
-	});
-
-});
-
-var key_c = ["C4", "D4", "E4", "F4", "G4", "A4", "B4"];
+var key_c = ["C4", "D4", "E4", "F4", "G4", "A4", "B4", "C4"];
 
 $('#LA_1').on("click", function() {
   console.log("LA Sound 1 Clicked");
@@ -51,59 +43,60 @@ $('#NYC_4').on("click", function() {
   synth.triggerAttackRelease("F4", "8n");
 });
 
-/*
- KICK
- */
-var kick = new Tone.MembraneSynth({
-  "envelope" : {
-    "sustain" : 0,
-    "attack" : 0.02,
-    "decay" : 0.8
-  },
-  "octaves" : 10
-}).toMaster();
-var kickPart = new Tone.Loop(function(time){
-  kick.triggerAttackRelease("C2", "8n", time);
-}, "2n").start(0);
+// /*
+//  KICK
+//  */
+// var kick = new Tone.MembraneSynth({
+//   "envelope" : {
+//     "sustain" : 0,
+//     "attack" : 0.02,
+//     "decay" : 0.8
+//   },
+//   "octaves" : 10
+// }).toMaster();
+// var kickPart = new Tone.Loop(function(time){
+//   kick.triggerAttackRelease("C2", "8n", time);
+// }, "2n").start(0);
+//
+// /*
+//  SNARE
+//  */
+// var snare = new Tone.NoiseSynth({
+//   "volume" : -5,
+//   "envelope" : {
+//     "attack" : 0.001,
+//     "decay" : 0.2,
+//     "sustain" : 0
+//   },
+//   "filterEnvelope" : {
+//     "attack" : 0.001,
+//     "decay" : 0.1,
+//     "sustain" : 0
+//   }
+// }).toMaster();
+// var snarePart = new Tone.Loop(function(time){
+//   snare.triggerAttack(time);
+// }, "2n").start("4n");
+// /**
+//  *  PIANO
+//  */
+// var piano = new Tone.PolySynth(4, Tone.Synth, {
+//   "volume" : -8,
+//   "oscillator" : {
+//     "partials" : [1, 2, 1],
+//   },
+//   "portamento" : 0.05
+// }).toMaster()
+// var cChord = ["C4", "E4", "G4", "B4"];
+// var dChord = ["D4", "F4", "A4", "C5"];
+// var gChord = ["B3", "D4", "E4", "A4"];
+// var pianoPart = new Tone.Part(function(time, chord){
+//   piano.triggerAttackRelease(chord, "8n", time);
+// }, [["0:0:2", cChord], ["0:1", cChord], ["0:1:3", dChord], ["0:2:2", cChord], ["0:3", cChord], ["0:3:2", gChord]]).start("2m");
+// pianoPart.loop = true;
+// pianoPart.loopEnd = "1m";
+// pianoPart.humanize = true;
 
-/*
- SNARE
- */
-var snare = new Tone.NoiseSynth({
-  "volume" : -5,
-  "envelope" : {
-    "attack" : 0.001,
-    "decay" : 0.2,
-    "sustain" : 0
-  },
-  "filterEnvelope" : {
-    "attack" : 0.001,
-    "decay" : 0.1,
-    "sustain" : 0
-  }
-}).toMaster();
-var snarePart = new Tone.Loop(function(time){
-  snare.triggerAttack(time);
-}, "2n").start("4n");
-/**
- *  PIANO
- */
-var piano = new Tone.PolySynth(4, Tone.Synth, {
-  "volume" : -8,
-  "oscillator" : {
-    "partials" : [1, 2, 1],
-  },
-  "portamento" : 0.05
-}).toMaster()
-var cChord = ["C4", "E4", "G4", "B4"];
-var dChord = ["D4", "F4", "A4", "C5"];
-var gChord = ["B3", "D4", "E4", "A4"];
-var pianoPart = new Tone.Part(function(time, chord){
-  piano.triggerAttackRelease(chord, "8n", time);
-}, [["0:0:2", cChord], ["0:1", cChord], ["0:1:3", dChord], ["0:2:2", cChord], ["0:3", cChord], ["0:3:2", gChord]]).start("2m");
-pianoPart.loop = true;
-pianoPart.loopEnd = "1m";
-pianoPart.humanize = true;
 /*
  BASS
  */
@@ -122,13 +115,32 @@ var bass = new Tone.MonoSynth({
     "octaves" : 2.6
   }
 }).toMaster();
-var bassPart = new Tone.Sequence(function(time, note){
-  bass.triggerAttackRelease(note, "16n", time);
-}, ["C2", ["C3", ["C3", "D2"]], "E2", ["D2", "A1"]]).start(0);
-bassPart.probability = 0.9;
+
+
+$.getJSON( "/sample_data.json", function( data ) {
+
+	var str = "";
+	var notes = [];
+	$.each( data.list, function( key, val ) {
+		if(key < 8) {
+			var index = Math.round((val.main.temp % 270) / 3);
+			str += key_c[index] + ",";
+			notes.push(key_c[index]);
+		}
+	});
+	
+	console.log(notes);
+	
+	var bassPart = new Tone.Sequence(function(time, note){
+	  bass.triggerAttackRelease(note, "16n", time);
+	}, notes).start(0);
+	// bassPart.probability = 0.9;
+	
+});
 
 var playing = false;
 $('#NOLA_play').on("click", function() {
+	
   Tone.Transport.bpm.value = 90;
   if (playing) {
     Tone.Transport.stop();
@@ -140,3 +152,7 @@ $('#NOLA_play').on("click", function() {
   }
 
 });
+
+
+
+
