64f93d9e41b1749332064bdce305b526057cf253
watson boyett <watson@lightandhologram.com>
2019-01-15 21:17:39 -0800

    Refactored a little bit.

--- js/main.js
+++ js/main.js
@@ -32,10 +32,6 @@ var piano = new Tone.PolySynth(4, Tone.Synth, {
   "portamento" : .05
 }).toMaster();
 
-var keys = {
-  'C': ["C", "D", "E", "F", "G", "A", "B", "C"]
-}
-
 var bass = new Tone.MonoSynth({
   "volume" : -20,
   "envelope" : {
@@ -53,63 +49,24 @@ var bass = new Tone.MonoSynth({
 }).toMaster();
 
 
-var LA_notes = [];
-var LA_words = [];
-var NOLA_notes = [];
-var NOLA_words = [];
-var NYC_notes = [];
-var NYC_words = [];
-
-
-var temp_min = 0;
-var temp_max = 100;
-var octave_min = 2;
-var octave_max = 7;
-var note_min = 0;
-var note_max = 100;
-
-var generateNotes = function(key, val, data_city_notes, data_city_words) {
-  if(key < 8) {
-    var temp_norm = (val.temp - temp_min) / (temp_max - temp_min);
-
-    var octave = temp_norm * (octave_max - octave_min) + octave_min;
-    octave = Math.floor(octave);
-
-    var index = temp_norm * (note_max - note_min) + note_min;
-    index = Math.floor(index % 8);
-
-    var note = keys['C'][index] + octave;
-    data_city_notes.push(note);
-
-    data_city_notes.push(index);
-    data_city_words.push(val.description);
-  }
+var keys = {
+  'C': ["C", "D",  "E",  "F",  "G",  "A",  "B",  "C"],
+  'D': ["D", "E",  "F#", "G",  "A",  "B",  "C#", "D"],
+  'E': ["E", "F#", "G#", "A",  "B",  "C#", "D#", "E"],
+  'F': ["F", "G",  "A",  "Bb", "C",  "D",  "E",  "F"],
+  'G': ["G", "A",  "B",  "C",  "D",  "E",  "F#", "G"],
+  'A': ["A", "C",  "C#", "D",  "E",  "F#", "G#", "A"],
+  'B': ["B", "C#", "D#", "E",  "F#", "G#", "A#", "B"],
 }
 
-$.getJSON( "data/today.json", function( data ) {
-  var str = "";
 
-	$.each( data[0].forecast, function( key, val ) {
-    generateNotes(key, val, LA_notes, LA_words);
-	});
-  console.log(LA_notes);
-
-  $.each( data[1].forecast, function( key, val ) {
-    generateNotes(key, val, NOLA_notes, NOLA_words);
-  });
-  console.log(NOLA_notes);
-
-  $.each( data[2].forecast, function( key, val ) {
-    generateNotes(key, val, NYC_notes, NYC_words);
-  });
-  console.log(NYC_notes);
-
-});
 
 var pianoPart;
 var kickPart;
 
-var playSong = function(city_button, city_notes, city_bpm, city_playing, city_words, city_voice) {
+var playSong = function(city_button, city_obj) {
+
+  let notes = city_obj['notes'];
 
   $(city_button).on("click", function() {
     var element = $(this);
@@ -125,13 +82,13 @@ var playSong = function(city_button, city_notes, city_bpm, city_playing, city_wo
 
     var bassPart = new Tone.Sequence(function(time, note){
       bass.triggerAttackRelease(note, "16n", time);
-    }, city_notes);
+    }, notes);
     bassPart.start(0);
 
     //
-    var cChord = [city_notes[0]];
-    var dChord = [city_notes[1]];
-    var gChord = [city_notes[2]];
+    var cChord = [notes[0]];
+    var dChord = [notes[1]];
+    var gChord = [notes[2]];
 
     pianoPart = new Tone.Part(function(time, chord) {
       piano.triggerAttackRelease(chord, "16n", time);
@@ -141,46 +98,104 @@ var playSong = function(city_button, city_notes, city_bpm, city_playing, city_wo
     pianoPart.loopEnd = "1m";
     pianoPart.humanize = true;
     pianoPart.start(0);
-    //
 
     //
     var snarePart = new Tone.Loop(function(time){
       snare.triggerAttack(time);
     }, "2n");
     snarePart.start(0);
-    //
 
     //
     kickPart = new Tone.Loop(function(time){
-      console.log(element);
       kick.triggerAttackRelease("C2", "8n", time);
+      
+      // visualize the kick
       element.closest('.city').find('.kick').addClass("active");
       setTimeout(function(){ element.closest('.city').find('.kick').removeClass("active"); }, 150);
     }, "2n");
     kickPart.start(0);
+    
     //
-
-    Tone.Transport.bpm.value = city_bpm;
-    if (city_playing) {
+    Tone.Transport.bpm.value = city_obj['bpm'];
+    if (city_obj['playing']) {
       Tone.Transport.stop();
       $(this).text("Play");
-      city_playing = false;
+      city_obj['playing'] = false;
     } else {
       Tone.Transport.start("+0.1");
       synth.triggerAttackRelease();
       setTimeout(function(){
-        responsiveVoice.speak(city_words[1], city_voice);
+        responsiveVoice.speak(city_obj['words'][1], city_obj['voice']);
       }, 1600);
-      city_playing = true;
+      city_obj['playing'] = true;
       $(this).text("Stop");
     }
   });
 };
 
-var LA_playing = false;
-var NOLA_playing = false;
-var NYC_playing = false;
 
-playSong('#LA_play', LA_notes, 100, LA_playing, LA_words,"UK English Male");
-playSong('#NOLA_play', NOLA_notes, 85, NOLA_playing, NOLA_words,"UK English Female");
-playSong('#NYC_play', NYC_notes, 120, NYC_playing, NYC_words,"US English Male");
+var temp_min = 0;
+var temp_max = 100;
+var octave_min = 2;
+var octave_max = 7;
+var note_min = 0;
+var note_max = 100;
+
+var generateNotes = function(forecast_data, city_obj) {
+  
+  $.each( forecast_data, function( idx, val ) {
+    if(idx < 8) {
+      var temp_norm = (val.temp - temp_min) / (temp_max - temp_min);
+
+      var octave = temp_norm * (octave_max - octave_min) + octave_min;
+      octave = Math.floor(octave);
+
+      var index = temp_norm * (note_max - note_min) + note_min;
+      index = Math.floor(index % 8);
+
+      var note = keys['C'][index] + octave;
+      city_obj['notes'].push(note);
+
+      //data_city_notes.push(index);
+      city_obj['words'].push(val.description);
+    }
+  });
+
+  console.log(city_obj['notes']);
+}
+
+var LA = {
+  'playing': false,
+  'notes': [],
+  'bpm': 100,
+  'voice': "UK English Male",
+  'words': [],
+};
+
+var NOLA = {
+  'playing': false,
+  'notes': [],
+  'bpm': 85,
+  'voice': "UK English Female",
+  'words': [],
+};
+
+var NYC = {
+  'playing': false,
+  'notes': [],
+  'bpm': 120,
+  'voice': "US English Male",
+  'words': [],
+};
+
+$.getJSON( "data/today.json", function( data ) {
+
+  generateNotes(data[0].forecast, LA);
+  generateNotes(data[1].forecast, NOLA);
+  generateNotes(data[2].forecast, NYC);
+
+});
+
+playSong('#LA_play', LA);
+playSong('#NOLA_play', NOLA);
+playSong('#NYC_play', NYC);
